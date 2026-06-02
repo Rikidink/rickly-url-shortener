c80c279b54f4714c58b80a62a3a1effe1b7db7a0
Ricky Z <rickyzhang2013@hotmail.com>
2026-06-02 19:45:11 +1000

    API cors

--- packages/api/src/app.ts
+++ packages/api/src/app.ts
@@ -1,10 +1,21 @@
 import { Hono } from 'hono'
 import type { Db } from './db'
 import { dbMiddleware } from './db'
+import { cors } from 'hono/cors';
 
 // attach bindings and db 
 const app = new Hono<{ Bindings: Env; Variables: { db: Db } }>();
+
 app.use('*', dbMiddleware);
+
+app.use('/api/*', cors({
+  origin: 'http://localhost:5173',
+  credentials: true,
+  allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  allowHeaders: ['Content-Type', 'Authorization'],
+  exposeHeaders: ['Content-Length'],
+  maxAge: 600,
+}));
 
 const testRoute = app.get('/', (c) => c.text('Hello Cloudflare Workers!'));
 
